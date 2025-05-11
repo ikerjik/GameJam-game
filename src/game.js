323563c8397f4fad3d544c1117d8ef271df1a2bd
nilov <nilov@glavweb.ru>
2025-05-11 22:10:06 +0700

    Удалена папка node_modules из git и добавлена в gitignore.

--- src/game.js
+++ src/game.js
@@ -1,11 +1,9 @@
 import {Game} from 'jetcode-scrubjs';
 
-import {MainStage} from './stages/main.stage.js';
 import {Dangeon} from "./stages/dangeon.stage";
 
 export const game = new Game(768, 768);
 
-const mainStage = new MainStage();
 const dangeon = new Dangeon();
 
 game.run(dangeon);
